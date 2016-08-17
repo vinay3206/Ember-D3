6d7b1aa4cfc41fb4ce857656f364725e003dd4d5
Sriram Narasimhan <Sriram.Narasimhan2@sc.com>
2016-08-18 01:39:24 +0800

    [sriram] d3 bar chart using ember basic version

--- app/components/d3-bar-chart.js
+++ app/components/d3-bar-chart.js
@@ -1,25 +1,42 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  height: 25,
+  width: 320,
+  tagName: 'svg',
+  classNames: ['bar-chart'],
+
   didInsertElement(){
-    this._super(...arguments);
-    var _chartData = [4, 8, 15, 16, 23, 42];
-
+    this._super(... arguments);
+    var _height = this.get('height');
     var x = d3.scaleLinear()
-      .domain([0, d3.max(_chartData)])
-      .range([0, 420]);
+      .domain([0, d3.max(this.get('chartData'))])
+      .range([0, this.get('width')]);
+    var chartCanvas = d3.select(this.$()[0])
+      .attr("width", this.get('width'))
+      .attr("height", this.get('height') * this.get('chartData').length);
 
-    var chart = d3.select(this.$('bar-chart')[0]);
-    var bar = chart.selectAll('div');
-    var barUpdate = bar.data(_chartData);
-    var barEnter = barUpdate.enter().append($('div'));
-    barEnter.style('width', function (d) {
-      return x(d) + "px";
-    });
-    barEnter.text(function (d) {
-      return d;
-    });
-    console.log(barEnter);
-    return barEnter.exit().remove();
+    var barChart = chartCanvas
+      .selectAll('g')
+      .data(this.get('chartData'));
+
+    var bar = barChart.enter().append('g')
+      .attr("transform", function (d, i) {
+        return "translate(0," + i * _height + ")";
+      });
+
+    bar.append("rect")
+      .attr("width", x)
+      .attr("height", this.get('height') - 1);
+
+    bar.append("text")
+      .attr("x", function (d) {
+        return x(d) - 3;
+      })
+      .attr("y", this.get('height') / 2)
+      .attr("dy", ".30em")
+      .text(function (d) {
+        return d;
+      });
   }
 });
--- app/routes/bar-chart.js
+++ app/routes/bar-chart.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  data: [4, 8, 15, 16, 23, 42],
+  data: [4, 8, 15, 16, 23, 42, 99, 120, 35, 36, 34,278],
   setupController (controller, model){
     this._super(controller, model);
     controller.setProperties({

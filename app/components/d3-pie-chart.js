b16aec4f65f3551b61730c3d0dea890b6ed7de4b
vinay pandey <AntiMatter@vinays-MacBook-Air.local>
2016-08-21 00:07:29 +0800

    added legends to pie chart and changed layout of home page.Included materialize css for row col grid

--- app/components/d3-pie-chart.js
+++ app/components/d3-pie-chart.js
@@ -13,8 +13,22 @@ export default Ember.Component.extend({
   donutWidth: 75,
   legendRectSize: 18,
   legendSpacing: 4,
-  radius: Ember.computed('height', 'width', function () {
-    return Math.min(this.get('width'), this.get('height')) / 2;
+  legend: true,
+  heightOffset: 30,
+  widthOffset: 100,
+  canvasHeight: Ember.computed('height', 'legend', function () {
+    let height = this.get('height') + this.get('heightOffset');
+    return height;
+  }),
+  canvasWidth: Ember.computed('width', 'legend', function () {
+    let width = this.get('width');
+    if(this.get('width')){
+      width += this.get('widthOffset');
+    }
+    return width;
+  }),
+  radius: Ember.computed('width', 'height', function () {
+    return (Math.min(this.get('width'), this.get('height'))) / 2;
   }),
 
   didInsertElement() {
@@ -24,8 +38,8 @@ export default Ember.Component.extend({
       d3.scaleOrdinal().range(this.get('colors')) : d3.scaleOrdinal(d3.schemeCategory20b);
 
     var group = d3.select(this.$()[0])
-      .attr('width', this.get('width'))
-      .attr('height', this.get('height'))
+      .attr('width', this.get('canvasWidth'))
+      .attr('height', this.get('canvasHeight'))
       .append('g');
 
     let chart = group.attr('transform',
@@ -48,27 +62,28 @@ export default Ember.Component.extend({
       .attr('fill', (d) => {
         return color(Ember.get(d.data, this.get('labelAttr')));
       });
-
-    let legend = chart.selectAll('legend')
-      .data(color.domain())
-      .enter()
-      .append('g')
-      .atr('class','legend')
-      .attr('transform',(d,i) => {
-        let height = this.get('legendRectSize') + this.get('legendSpacing');
-        let offset = height * color.domain().length / 2;
-        var horz = -2 * this.get('legendRectSize');
-        var vert = i * height - offset ;
-        return `translate(${horz},${vert})`;
-      });
-    legend.append('rect')
-      .attr('width',this.get('legendRectSize'))
-      .attr('height',this.get('legendRectSize'))
-      .style('fill',color)
-      .style('stroke',color);
-    legend.append('text')
-      .attr('x',this.get('legendRectSize') + this.get('legendSpacing'))
-      .attr('y',this.get('legendRectSize') - this.get('legendSpacing'))
-      .text((d) => d);
+    if(this.get('legend')){
+        let legend = chart.selectAll('.legend')
+          .data(color.domain())
+          .enter()
+          .append('g')
+          .attr('class','legend')
+          .attr('transform',(d,i) => {
+            let height = this.get('legendRectSize') + this.get('legendSpacing');
+            let offset = height * color.domain().length / 2;
+            var horz =  this.get('radius') + 20;
+            var vert = i * height - offset ;
+            return `translate(${horz},${vert})`;
+          });
+        legend.append('rect')
+          .attr('width',this.get('legendRectSize'))
+          .attr('height',this.get('legendRectSize'))
+          .style('fill',color)
+          .style('stroke',color);
+        legend.append('text')
+          .attr('x',this.get('legendRectSize') + this.get('legendSpacing'))
+          .attr('y',this.get('legendRectSize') - this.get('legendSpacing'))
+          .text((d) => d);
+    }
   }
 });
